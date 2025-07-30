f03818a0a733320741f2a3afc1ed05951aebf49f
Franciszek Janicki <franciszek.janicki1@gmail.com>
2025-07-30 13:23:58 +0200

    changes

--- bus_task.h
+++ bus_task.h
@@ -8,17 +8,21 @@
 #include <stdint.h>
 
 typedef struct {
-  bus_config_t config;
-  bus_interface_t interface;
+    bus_config_t config;
+    bus_interface_t interface;
 } bus_task_ctx_t;
 
-TaskHandle_t
-bus_task_create_task(bus_task_ctx_t *task_ctx, char const *task_name,
-                     StaticTask_t *task_buffer, UBaseType_t task_priority,
-                     StackType_t *task_stack, UBaseType_t task_stack_size);
+TaskHandle_t bus_task_create_task(bus_task_ctx_t* task_ctx,
+                                  char const* task_name,
+                                  StaticTask_t* task_buffer,
+                                  UBaseType_t task_priority,
+                                  StackType_t* task_stack,
+                                  UBaseType_t task_stack_size);
 StreamBufferHandle_t bus_task_create_stream_buffer(
-    StaticStreamBuffer_t *stream_buffer, UBaseType_t stream_buffer_trigger,
-    UBaseType_t stream_buffer_storage_size, uint8_t *stream_buffer_storage);
+    StaticStreamBuffer_t* stream_buffer,
+    UBaseType_t stream_buffer_trigger,
+    UBaseType_t stream_buffer_storage_size,
+    uint8_t* stream_buffer_storage);
 
 void bus_task_receive_done_callback(TaskHandle_t bus_task);
 void bus_task_transmit_done_callback(TaskHandle_t bus_task);
